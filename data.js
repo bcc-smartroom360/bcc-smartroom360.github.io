0726db236f425c4c0aad78fd42e55d50a7de78b2
nai27 <agtarapian27@gmail.com>
2023-11-29 14:54:04 +0800

    first commit

---
+++ data.js
@@ -0,0 +1,330 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-to-masters-bedroom",
+      "name": "To Master's Bedroom",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": -0.6939205870033707,
+        "pitch": -0.05315419726804116,
+        "fov": 1.4537902127120284
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.734795229520918,
+          "pitch": 0.16749731853576932,
+          "rotation": 0,
+          "target": "1-to-living-room"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.9977697398808818,
+          "pitch": -0.7026324603963836,
+          "title": "Master Bedroom Cove Lights",
+          "text": "Alexa, turn on/off master room cove lights."
+        },
+        {
+          "yaw": -2.124839818971836,
+          "pitch": -0.1725477041632928,
+          "title": "Master Bedroom Artifact Cove Lights",
+          "text": "Alexa, turn on/off master room artifact cove lights."
+        },
+        {
+          "yaw": -1.7724645445733156,
+          "pitch": -0.15865069702154244,
+          "title": "Master Bedroom Artifact Lights",
+          "text": "Alexa, turn on/off master room artifact lights."
+        },
+        {
+          "yaw": -0.7955101679247125,
+          "pitch": -0.9428187797722298,
+          "title": "Master Bedroom Ceiling Fan",
+          "text": "Alexa, turn on/off master room ceiling fan."
+        },
+        {
+          "yaw": -0.3613447544523751,
+          "pitch": -0.5166334172492846,
+          "title": "Master Bedroom Main Lights",
+          "text": "Alexa, turn on/off master room main lights."
+        },
+        {
+          "yaw": 0.8706437112658953,
+          "pitch": -0.4959683706438103,
+          "title": "Master Bedroom Desk Lights",
+          "text": "Alexa, turn on/off master room desk lights."
+        },
+        {
+          "yaw": 0.5609585495366574,
+          "pitch": -0.1088026937785962,
+          "title": "Master Bedroom Curtain 1",
+          "text": "Alexa, close/open master room curtain 1."
+        },
+        {
+          "yaw": -0.36979575567107936,
+          "pitch": 0.005757712380873414,
+          "title": "Master Bedroom Curtain 2",
+          "text": "Alexa, close/open master room curtain 2."
+        },
+        {
+          "yaw": -1.0344616494388408,
+          "pitch": -0.14349238418602184,
+          "title": "Master Bedroom Balcony Lights",
+          "text": "Alexa, turn on/off master room balcony lights."
+        },
+        {
+          "yaw": 0.07449347693758313,
+          "pitch": 0.16674585063421432,
+          "title": "Master Bedroom TV",
+          "text": "Alexa, turn on/off master bedroom TV."
+        },
+        {
+          "yaw": 2.5405728921412436,
+          "pitch": -0.5469093807036298,
+          "title": "Master Bedroom Foyer Lights",
+          "text": "Alexa, turn on/off master room foyer lights."
+        },
+        {
+          "yaw": 2.911449929427496,
+          "pitch": -0.4327999381420984,
+          "title": "Master Bedroom Dresser Lights",
+          "text": "Alexa, turn on/off master room dresser lights."
+        }
+      ]
+    },
+    {
+      "id": "1-to-living-room",
+      "name": "To Living Room",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": 0.5678070773605093,
+        "pitch": 0.13298186915242205,
+        "fov": 1.4537902127120284
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.0916153309157366,
+          "pitch": 0.07006046116449838,
+          "rotation": 0,
+          "target": "2-to-twin-bedroom"
+        },
+        {
+          "yaw": 2.2540691485815794,
+          "pitch": 0.0731513215641062,
+          "rotation": 0,
+          "target": "0-to-masters-bedroom"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.1089928273917309,
+          "pitch": -0.3675143232612079,
+          "title": "Living Room Desk Lights",
+          "text": "Alexa, turn on/off living room desk lights."
+        },
+        {
+          "yaw": 0.5209293115001046,
+          "pitch": -0.7607776569797551,
+          "title": "Living Room Ceiling Fan",
+          "text": "Alexa, turn on/off living room ceiling fan."
+        },
+        {
+          "yaw": 1.2533884290980914,
+          "pitch": -0.9595920162809257,
+          "title": "Living Room Main Lights",
+          "text": "Alexa, turn on/off living room main lights."
+        },
+        {
+          "yaw": 2.139315891912098,
+          "pitch": -0.7735623296878789,
+          "title": "Living Room Cove Lights",
+          "text": "Alexa, turn on/off living room cove lights."
+        },
+        {
+          "yaw": 1.3327885959444359,
+          "pitch": 0.11226214833556014,
+          "title": "Living Room TV",
+          "text": "Alexa, turn on/off living room TV."
+        },
+        {
+          "yaw": 0.34701230864303767,
+          "pitch": -0.22856069219901975,
+          "title": "Living Room Balcony Lights",
+          "text": "Alexa, turn on/off living room balcony lights."
+        },
+        {
+          "yaw": 0.10274646382438668,
+          "pitch": -0.043208569651822515,
+          "title": "Living Room Curtain",
+          "text": "Alexa, open/close living room curtain."
+        },
+        {
+          "yaw": -2.504151775434149,
+          "pitch": -0.6240600853800764,
+          "title": "Living Room Foyer Lights",
+          "text": "Alexa, turn on/off living room foyer lights."
+        },
+        {
+          "yaw": -2.197267146085249,
+          "pitch": -0.2010767597458809,
+          "title": "Living Room Minibar Lights",
+          "text": "Alexa, turn on/off living room minibar lights."
+        }
+      ]
+    },
+    {
+      "id": "2-to-twin-bedroom",
+      "name": "To Twin Bedroom",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.2067904740991446,
+          "pitch": 0.06545487731313315,
+          "rotation": 0,
+          "target": "1-to-living-room"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.698995808196541,
+          "pitch": -1.1674434777690408,
+          "title": "Twin Bedroom Ceiling Fan",
+          "text": "Alexa, turn on/off twin room&nbsp;ceiling fan."
+        },
+        {
+          "yaw": -2.369487324186231,
+          "pitch": -0.6646199384943188,
+          "title": "Main Lights",
+          "text": "Alexa, turn on/off twin room main lights."
+        },
+        {
+          "yaw": -1.5045301264279836,
+          "pitch": -0.10073416868122465,
+          "title": "Artifact Cove Lights",
+          "text": "Alexa, turn on/off twin room artifact cove lights."
+        },
+        {
+          "yaw": -2.0963953920677465,
+          "pitch": 0.01283981875180551,
+          "title": "Artifact Lights",
+          "text": "Alexa, turn on/off twin room artifact lights."
+        },
+        {
+          "yaw": 2.9286093425256565,
+          "pitch": -0.34315656957673824,
+          "title": "Twin Bedroom Foyer Lights",
+          "text": "Alexa turn on/off twin room foyer lights."
+        },
+        {
+          "yaw": 2.663670414167777,
+          "pitch": 0.06783868823103845,
+          "title": "Twin Bedroom Minibar Lights",
+          "text": "Alexa, turn on/off twin room minibar lights."
+        },
+        {
+          "yaw": 0.756110375413451,
+          "pitch": -0.5330784187719466,
+          "title": "Twin Bedroom Desk Lights",
+          "text": "Alexa, turn on/off twin room desk lights."
+        },
+        {
+          "yaw": 1.2536707004892715,
+          "pitch": 0.17828428474780722,
+          "title": "Twin Bedroom TV",
+          "text": "Alexa, turn on/off twin room TV."
+        },
+        {
+          "yaw": 0.1893601024620981,
+          "pitch": -0.28130373109705786,
+          "title": "Twin Bedroom Balcony Lights",
+          "text": "Alexa, turn on/off twin room balcony lights."
+        },
+        {
+          "yaw": -0.7644029597014068,
+          "pitch": 0.02838487000507861,
+          "title": "Twin Bedroom Curtain",
+          "text": "Alexa, open/close twin room curtain."
+        },
+        {
+          "yaw": -1.5839413961443825,
+          "pitch": -0.5570042319876354,
+          "title": "Cove Lights",
+          "text": "Alexa, turn on/off twin room cove lights."
+        }
+      ]
+    }
+  ],
+  "name": "Smart Room",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
